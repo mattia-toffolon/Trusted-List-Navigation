ba49d645b910766d09feed33546f762a5588b2ab
powerT64 <58696665+powerT64@users.noreply.github.com>
2022-05-29 22:43:55 +0200

    Ora si vede la lista dei tipi servizi

--- src/main/java/proseccoCoding/TLN/control/ListServicesController.java
+++ src/main/java/proseccoCoding/TLN/control/ListServicesController.java
@@ -1,9 +1,13 @@
 package proseccoCoding.TLN.control;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.util.Pair;
 import proseccoCoding.TLN.App;
+import proseccoCoding.TLN.model.APIHandler;
 
 /**
  * 
@@ -18,6 +22,16 @@ public class ListServicesController {
 	 */
 	private ListView servicesList;
 	
+	@FXML
+	/**
+	 * Method called when ListServicesController is loaded. 
+	 * It adds the list of the service type to the private ListView object
+	 */
+	private void initialize() {
+    	// add countries to the ListView
+		for (String s : requestServiceTypes())
+			servicesList.getItems().add(s);
+	}
     @FXML
     /**
      * Switches scene to the "home" one
@@ -26,4 +40,19 @@ public class ListServicesController {
     private void switchToHome() throws IOException {
         App.setRoot("home");
     }
+    
+    /**
+     * Private method that calls APIHandler.requestServiceTypes(), which connects to an API that returns a 
+     * .json file containing the list of the service types. That method parses that file to create and then return
+     *  an ArrayList of strings containing those types.
+     * @return ArrayList<String> containing all the service types
+     */
+    private static ArrayList<String> requestServiceTypes() {
+    	ArrayList<String> ret = new ArrayList<String>();
+		ArrayList<Pair<String,String>> list = APIHandler.retriveServiceTypes();
+		for(Pair<String,String> p : list){
+			ret.add(p.getValue());
+		}
+		return ret;
+	}
 }
