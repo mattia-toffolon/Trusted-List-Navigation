5b49457f224bc6157db08d1fc172c8ba4ea31a8e
powerT64 <58696665+powerT64@users.noreply.github.com>
2022-05-29 22:45:22 +0200

    aggiornato requestCountries sfruttando APIHANDLER

--- src/main/java/proseccoCoding/TLN/control/ListCountriesController.java
+++ src/main/java/proseccoCoding/TLN/control/ListCountriesController.java
@@ -16,6 +16,7 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import proseccoCoding.TLN.App;
+import proseccoCoding.TLN.model.APIHandler;
 
 /**
  * 
@@ -51,47 +52,13 @@ public class ListCountriesController {
     }
     
     /**
-     * Private method that connects to an API that returns a .json file containing the list of the countries. 
-     * This method parses that file to create and then return an ArrayList of strings containing those names.
+     * Private method that calls APIHandler.retriveCountriesNames(), which connects to an API that returns a 
+     * .json file containing the list of the countries. That method parses that file to create and then 
+     * return an ArrayList of strings containing those names.
      * @return ArrayList<String> containing all the countries names
      */
     private static ArrayList<String> requestCountries() {
-		ArrayList<String> countries = new ArrayList<String>();
-		
-		try {
-			// try connection
-			URL url;
-			url = new URL("https://esignature.ec.europa.eu/efda/tl-browser/api/v1/search/countries_list");
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-			
-			// create json object
-			JSONArray jsonArr = new JSONArray();
-			
-			int responseCode = connection.getResponseCode();
-			System.out.println("GET Response Code :: " + responseCode);
-			
-			// if get request status is OK
-			if (responseCode == HttpURLConnection.HTTP_OK) {
-				// read from response body
-				BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-				// parse response body json
-				jsonArr = new JSONArray(new JSONTokener(in));
-				in.close();
-			}
-			// iterate over parsed json and add countries names to the ArrayList
-			Iterator<Object> it = jsonArr.iterator();
-			while (it.hasNext()) {
-				JSONObject object = (JSONObject) it.next();
-				countries.add((String) object.get("countryName"));		
-			}
-			
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		ArrayList<String> countries = APIHandler.retrieveCountriesNames();
 		return countries;
 	}
 }
