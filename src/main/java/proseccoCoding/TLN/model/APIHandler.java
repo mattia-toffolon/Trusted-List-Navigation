1e1b619238014666976d9c755ef7de584bbc46ff
mattia-toffolon <mattia.toffolon@studenti.unipd.it>
2022-05-31 12:39:15 +0200

    Bug ServiceType risolto

--- src/main/java/proseccoCoding/TLN/model/APIHandler.java
+++ src/main/java/proseccoCoding/TLN/model/APIHandler.java
@@ -215,6 +215,8 @@ public class APIHandler {
 				Provider tempProvider = new Provider((String) obj.get("name"), tempCountry);
 				JSONArray services = obj.getJSONArray("services");
 
+				System.out.println("services num: " + services.length());
+				
 				// Build each service object with status, name service and country code. After
 				// that it is put provider's Multimap
 				for (int i = 0; i < services.length(); i++) { // loop for every service
@@ -222,11 +224,10 @@ public class APIHandler {
 					
 					String tempName = temp.getString("serviceName");
 					// in the json is an array, but it actually always have 1 element
-					String tempTypeCode = obj.getJSONArray("qServiceTypes").getString(0);
+					String tempTypeCode = temp.getJSONArray("qServiceTypes").getString(0);
 					ServiceType tempServiceType = ServiceType.getInstance(tempTypeCode);
 					String tempStatus = temp.getString("currentStatus"); // complete string
 					tempStatus = tempStatus.substring(50, tempStatus.length()); // only the status
-					
 					Service tempService = new Service(tempName,tempServiceType,tempStatus,tempProvider);
 					tempProvider.addService(tempService);
 					}
