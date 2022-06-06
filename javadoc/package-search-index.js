cacfec797f49ca961ca06067c47a919420635fef
mattia-toffolon <96696061+mattia-toffolon@users.noreply.github.com>
2022-06-06 09:11:27 +0200

    Add files via upload

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"proseccoCoding.TLN.control"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"proseccoCoding.TLN.control"}];updateSearchResults();
